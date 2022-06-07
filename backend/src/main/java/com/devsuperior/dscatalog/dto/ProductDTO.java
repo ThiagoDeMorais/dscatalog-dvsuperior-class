b79bb6e6f9447e4b48ac5cc29cf28899a2d67bbb
Thigo De Morais <demoraisthiago63@gmail.com>
2022-06-07 17:43:26 -0300

    ProductDTO

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscatalog.dto;
 
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
@@ -13,25 +17,36 @@ public class ProductDTO implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	private Category category;
+	private Instant date;
+	
+	private List<CategoryDTO> categories = new ArrayList<>();
 	
 	public ProductDTO() {
 		
 	}
 
-	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Category category) {
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
-		this.category = category;
+		this.date = date;
 	}
 
 	public ProductDTO(Product entity) {
-		this.id =entity.getId();
+		this.id = entity.getId();
 		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+		this.date = entity.getDate();
+	}
+	
+	public ProductDTO(Product entity, Set<Category> categories) {
+		this(entity);
+		categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
 	}
 
 	public Long getId() {
@@ -70,12 +85,16 @@ public class ProductDTO implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-	public Category getCategory() {
-		return category;
+	public Instant getDate() {
+		return date;
 	}
 
-	public void setCategory(Category category) {
-		this.category = category;
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public List<CategoryDTO> getCategories() {
+		return categories;
 	}
 
 	public static long getSerialversionuid() {
